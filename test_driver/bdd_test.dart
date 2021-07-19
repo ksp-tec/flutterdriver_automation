2380beb1f5ec8c5f635df963ca9aba18eea31c48
Syamphaneendra Kalluri <syamphaneendrak@gmail.com>
2021-07-19 13:16:51 +0100

    Initial commit after android update

--- test_driver/bdd_test.dart
+++ test_driver/bdd_test.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
+
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
 import 'package:glob/glob.dart';
-import 'steps/initial_state_of_app.dart';
+
 import 'steps/I_click_plus_step.dart';
-import 'steps/I_see_value_step.dart';
 import 'steps/I_click_subtract.dart';
+import 'steps/I_see_value_step.dart';
+import 'steps/initial_state_of_app.dart';
 
 Future<void> main() {
   final config = FlutterTestConfiguration()
@@ -15,10 +17,15 @@ Future<void> main() {
       TestRunSummaryReporter(),
       JsonReporter(path: './report.json')
     ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
-    ..stepDefinitions = [InitialStateOfApp(), ClickPlus(), ISeeValue(), ClickSubtract()]
+    ..stepDefinitions = [
+      InitialStateOfApp(),
+      ClickPlus(),
+      ISeeValue(),
+      ClickSubtract()
+    ]
     ..restartAppBetweenScenarios = true
-    ..targetAppPath = "test_driver/bdd.dart"
-    // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
-    ..exitAfterTestRun = true; // set to false if debugging to exit cleanly
+    ..targetAppPath = "test_driver/bdd.dart";
+  // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
+  //..exitAfterTestRun = true; // set to false if debugging to exit cleanly
   return GherkinRunner().execute(config);
 }
